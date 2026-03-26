72a5e377b4265276d84bb32e070aadf6d19f52e6
PrachiParkar1 <prachiparkar509@gmail.com>
2026-03-26 11:52:42 +0530

    Update streamlit_app.py
    addition of api

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,7 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+
 
 
 # Write directly to the app.
@@ -49,3 +47,6 @@ if ingredient_list:
         st.success('Your Smoothie is ordered! '+ name_on_order, icon="✅")
 
 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
